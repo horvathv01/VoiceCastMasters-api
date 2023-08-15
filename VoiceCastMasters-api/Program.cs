28db656000ee46000d396bd4c540184d37262eb7
horvathv01 <horvathv01@gmail.com>
2023-08-15 11:17:58 +0200

    add first endpoint for testing purposes

--- VoiceCastMasters-api/Program.cs
+++ VoiceCastMasters-api/Program.cs
@@ -1,6 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("LocalNetwork", 
+        builder => builder.WithOrigins("http://localhost:3000", "http://192.168.1.248:3000", "http://192.168.0.110:3000")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials()
+        );
+    options.AddPolicy("AllowAll", builder => builder.AllowAnyOrigin()
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+    );
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -15,6 +28,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("LocalNetwork");
 
 //app.UseHttpsRedirection();
 
