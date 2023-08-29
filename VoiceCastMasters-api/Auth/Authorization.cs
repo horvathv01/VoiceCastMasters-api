908d8fa1c1ca08a32fabb739441709b2f369bfa2
TaurusBoss <szova.kristof@gmail.com>
2023-08-29 10:35:02 +0200

    add HashPassword method

--- VoiceCastMasters-api/Auth/Authorization.cs
+++ VoiceCastMasters-api/Auth/Authorization.cs
@@ -1,8 +1,15 @@
-﻿namespace VoiceCastMasters_api.Auth;
+﻿using VoiceCastMasters_api.Model;
+
+namespace VoiceCastMasters_api.Auth;
 
 public class Authorization : IAuthorization
 {
     public bool Authorize()
+    {
+        throw new NotImplementedException();
+    }
+
+    public string HashPassword(User user, string passwordToHash)
     {
         throw new NotImplementedException();
     }
--- VoiceCastMasters-api/Auth/IAuthorization.cs
+++ VoiceCastMasters-api/Auth/IAuthorization.cs
@@ -1,6 +1,9 @@
-﻿namespace VoiceCastMasters_api.Auth;
+﻿using VoiceCastMasters_api.Model;
+
+namespace VoiceCastMasters_api.Auth;
 
 public interface IAuthorization
 {
     public bool Authorize();
+    public string HashPassword(User user, string passwordToHash);
 }
