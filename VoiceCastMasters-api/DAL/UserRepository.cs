c5943e28f6cccf6d46d7a67bff364ab7c51b002e
TaurusBoss <szova.kristof@gmail.com>
2023-09-12 09:56:06 +0200

    add UserRepository and implement "Add" method

---
+++ VoiceCastMasters-api/DAL/UserRepository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VoiceCastMasters_api.Model;
+
+namespace VoiceCastMasters_api.DAL;
+
+public class UserRepository : IRepository<User>
+{
+    private DbContext _databaseContext;
+    public UserRepository(DbContext databaseContext)
+    {
+        _databaseContext = databaseContext;
+    }
+    public bool Add(User entity)
+    {
+        try
+        {
+            _databaseContext.Add(entity);
+            _databaseContext.SaveChanges();
+            return true;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return false;
+        }
+        
+    }
+
+    public User GetById(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Update(long id, User entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Delete(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<User> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public User? GetByEmail(string email)
+    {
+        throw new NotImplementedException();
+    }
+}
